96c37a73b319a15f0b4cbd70f1fb0567eb3d176a
yuqilin <yuqilin@guoxiaomei.com>
2020-02-24 13:03:47 +0800

    Initial commit

---
+++ src/test/java/com/tututu/thinking/ThinkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tututu.thinking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
